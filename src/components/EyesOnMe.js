7fce2a8d00da9fce6c20fa2ebd89bbd74adc190d
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-12-02 08:16:10 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(event){
+    function handleFocus(){
+        console.log('Good!');
+    }
+    function handleBlur(event){
+        console.log ('Hey! Eyes on me!');
+    }
+    
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    )
+};
+
+export default EyesOnMe;
